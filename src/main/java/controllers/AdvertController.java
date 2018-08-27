e25b2b3d87a1d3af9d4edee150c5701d24ef1616
Ricardo Ruiz <rfpimpao@gmail.com>
2018-08-27 14:31:53 +0100

    added edit and update routes to adverts

--- src/main/java/controllers/AdvertController.java
+++ src/main/java/controllers/AdvertController.java
@@ -54,8 +54,52 @@ public class AdvertController {
             res.redirect("/adverts");
             return null;
         });
+        //edit
+        get("/adverts/:id/edit", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            Integer id = Integer.parseInt(req.params(":id"));
+            List<Category> categories = DBHelper.getAll(Category.class);
+            List<DeliveryOption> deliveryOptions = DBHelper.getAll(DeliveryOption.class);
+            Advert advert = DBHelper.find(id, Advert.class);
+
+            Set<DeliveryOption> advertDeliveryOptions = DBAdvert.findDeliveryOptionsByAdvert(advert);
+
+            model.put("advertDeliveryOptions", advertDeliveryOptions);
+            model.put("deliveryOptions", deliveryOptions);
+            model.put("categories", categories);
+            model.put("advert", advert);
+            model.put("template", "templates/adverts/edit.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+        //destroy(could be archive)
+
+        //update
+        post("/adverts/:id", (req, res) ->{
+            Integer id = Integer.parseInt(req.params(":id"));
+            Advert advert = DBHelper.find(id, Advert.class);
+            String title = req.queryParams("title");
+            String description = req.queryParams("description");
+            double askingPrice = Double.parseDouble(req.queryParams("askingPrice"));
+            int catId = Integer.parseInt(req.queryParams("category"));
+            Category category = DBHelper.find(catId, Category.class);
+            Set<String> allParams = req.queryParams();
+            Set<DeliveryOption> deliveryOptions = assignDeliveryOption(allParams);
+
+            advert.setTitle(title);
+            advert.setDescription(description);
+            advert.setAskingPrice(askingPrice);
+            advert.setCategory(category);
+            advert.setDeliveryOptions(deliveryOptions);
+            DBHelper.save(advert);
+            res.redirect("/adverts");
+            return null;
+        });
+
+
+
+
         //show
-        get("/adverts/:id", (req, res)->{
+        get("/adverts/:id", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             Integer id = Integer.parseInt(req.params(":id"));
             Advert advert = DBHelper.find(id, Advert.class);
@@ -67,14 +111,14 @@ public class AdvertController {
             model.put("template", "templates/adverts/show.vtl");
             return new ModelAndView(model, "templates/layout.vtl");
         }, new VelocityTemplateEngine());
-        //edit
-        //update
-        //destroy(could be archive)
+
+
+
     }
 
-    private List<Integer> getOptionsFromAllParams(Set<String> params){
+    private List<Integer> getOptionsFromAllParams(Set<String> params) {
         List<Integer> ids = new ArrayList<>();
-        for (String param: params){
+        for (String param : params) {
             if (param.contains("_option")) {
                 String[] slicedParam = param.split("_");
                 ids.add(Integer.parseInt(slicedParam[0]));
@@ -83,17 +127,17 @@ public class AdvertController {
         return ids;
     }
 
-    private Set<DeliveryOption> findOptionsOnDatabase(List<Integer> ids){
+    private Set<DeliveryOption> findOptionsOnDatabase(List<Integer> ids) {
         Set<DeliveryOption> deliveryOptions = new HashSet<>();
-        for (int id:ids){
+        for (int id : ids) {
             DeliveryOption deliveryOption = DBHelper.find(id, DeliveryOption.class);
             deliveryOptions.add(deliveryOption);
         }
         return deliveryOptions;
     }
 
-    private Set<DeliveryOption> assignDeliveryOption(Set<String> params){
-        List<Integer>ids = getOptionsFromAllParams(params);
+    private Set<DeliveryOption> assignDeliveryOption(Set<String> params) {
+        List<Integer> ids = getOptionsFromAllParams(params);
         return findOptionsOnDatabase(ids);
     }
 
